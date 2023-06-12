5bbf12cce72c689ce187052beca505d2e8699568
vori1987 <starosto@ua.fm>
2023-06-12 10:54:51 +0200

    added CreateStudentPayloadTest

---
+++ src/test/java/com/studentapp/tests/CreateStudentPayloadTest.java
@@ -0,0 +1,25 @@
+package com.studentapp.tests;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class CreateStudentPayloadTest extends TestBase {
+
+    @DisplayName("Create a new student by sending payload as String")
+    @Test
+    void createNewStudent() {
+        String payload = "{\"firstName\":\"test3\",\"lastName\":\"User\",\"email\":\"test3@gmail.com\",\"programme\":\"ComputerScience\",\"courses\":[\"C++\",\"JAVA\"]}";
+
+        given().
+                when().
+                contentType(ContentType.JSON).
+                when().
+                body(payload).post().then().statusCode(201);
+
+
+
+    }
+}
